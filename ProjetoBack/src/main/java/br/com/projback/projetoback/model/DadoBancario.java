dc5391a77cdd3f00174cbb83121a2206c75a3345
Giovanelli <joaopedrogiovanelli@gmail.com>
2024-04-19 08:11:05 -0300

    colocando o id no banco

--- ProjetoBack/src/main/java/br/com/projback/projetoback/model/DadoBancario.java
+++ ProjetoBack/src/main/java/br/com/projback/projetoback/model/DadoBancario.java
@@ -1,11 +1,19 @@
 package br.com.projback.projetoback.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class DadoBancario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int banco_id;
+
     private String codigoBanco;
     private String agencia;
     private String conta;
