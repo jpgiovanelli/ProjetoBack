2b50b8c269dce6cf5701e8fe4a5b10d4f01f0445
João Pedro Giovanelli <joaopedrogiovanelli@gmail.com>
2024-06-20 09:20:26 -0300

    COLOCANDO LojistaException pra pegar com ExceptionHandler - resolvendo erro

--- ProjetoBack/src/main/java/br/com/projback/projetoback/exception/LojistaException.java
+++ ProjetoBack/src/main/java/br/com/projback/projetoback/exception/LojistaException.java
@@ -2,14 +2,16 @@ package br.com.projback.projetoback.exception;
 
 public class LojistaException extends Exception{
     private String field;
+    private String message;
     public LojistaException(String field, String message){
         super(message);
         this.field = field;
+        this.message = message;
     }
     public String getField(){
         return this.field;
     }
     public String getMessage(){
-        return this.field;
+        return this.message;
     }
 }
